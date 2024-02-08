1591d4f9eeaf2f568e0121a1d8dbfa726164db51
Danielle Bassetto <danibbassetto@hotmail.com>
2024-02-08 19:52:52 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using ScreenSound.Shared.Modelos.Modelos;
+
+namespace ScreenSound.Banco;
+public class ScreenSoundContext: DbContext
+{
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Musica>()
+            .HasMany(c => c.Generos)
+            .WithMany(c => c.Musicas);
+    }
+
+}

933575e99ae75ab36fc51a6600374006dcd316cd
Danielle Bassetto <danibbassetto@hotmail.com>
2024-03-14 21:07:10 -0300

    new

--- ScreenSound.Web/Layout/ScreendsoundPallete.cs
+++ ScreenSound.Web/Layout/ScreendsoundPallete.cs
@@ -11,6 +11,5 @@ public sealed class ScreendsoundPallete : PaletteDark
         Secondary = new MudColor("#F6AD31");
         Tertiary = new MudColor("#8AE491");
     }
-
     public static ScreendsoundPallete CreatePallete => new();
 }
